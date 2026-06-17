ca99e900296f5816c8509a6700971dd30e2866dd
himadhar0812 <himadharmadupuri@gmail.com>
2026-06-17 23:14:12 +0530

    Create Longest Common Sub String.java

---
+++ Longest Common Sub String.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String a = s.next();
+         String b = s.next();
+         int dp[][]= new int[a.length()+1][b.length()+1];
+         int m=0;
+         for(int i=1;i<=a.length();i++){
+             for(int j=1;j<=b.length();j++){
+                 if(a.charAt(i-1) == b.charAt(j-1)){
+                 dp[i][j]=dp[i-1][j-1]+1;
+                 if(m<dp[i][j])
+                 m=dp[i][j];
+                }
+             }
+         }
+         System.out.println(m);
+    }
+}
